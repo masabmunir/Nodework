7ca2cb245a8e4471df608bff3b12cf4bf0f24347
Masab <masabk214@gmail.com>
2021-09-04 13:40:48 -0700

    routing of 404 error is added

--- src/app.js
+++ src/app.js
@@ -5,17 +5,17 @@ const port = 3000;
 
 
 const staticpath = path.join(__dirname,"website");
-const templatepath = path.join(__dirname,"../Templates")
-//app.use(express.static(staticpath));
+// const templatepath = path.join(__dirname,"../Templates")
+app.use(express.static(staticpath));
 
-app.set('view engine', 'hbs');
-app.set('views',templatepath)
+// app.set('view engine', 'hbs');
+// app.set('views',templatepath)
 
-app.get("",(req,res)=>{
-    res.render('index',{
-        personname:'Masab',
-    });
-})
+// app.get("",(req,res)=>{
+//     res.render('index',{
+//         personname:'Masab',
+//     });
+// })
 
 app.get("/",(req,res)=>{
    res.send("welcome to home page of my practice site");
@@ -26,6 +26,9 @@ app.get("/about",(req,res)=>{
     res.send("welcome to about page of my practice site");
 })
 
+app.get('*',(req,res)=>{
+  res.send("404 error occur");
+})
 
 app.listen(3000,(req,res)=>{
     console.log(`app is listening ${port}`)
