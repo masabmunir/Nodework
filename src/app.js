9c1c1204b3fa9540537e8719a8ebcceb10ac3e6a
Masab <masabk214@gmail.com>
2021-09-05 12:54:37 -0700

    changes done for nodejs query strings method

--- src/app.js
+++ src/app.js
@@ -5,25 +5,27 @@ const port = 3000;
 
 
 const staticpath = path.join(__dirname,"website");
-// const templatepath = path.join(__dirname,"../Templates")
-app.use(express.static(staticpath));
+const templatepath = path.join(__dirname,"../Templates")
+//app.use(express.static(staticpath));
 
-// app.set('view engine', 'hbs');
-// app.set('views',templatepath)
+app.set('view engine', 'hbs');
+app.set('views',templatepath)
 
-// app.get("",(req,res)=>{
-//     res.render('index',{
-//         personname:'Masab',
-//     });
-// })
+app.get("",(req,res)=>{
+    res.render('index',{
+        personname:'Masab',
+        random:req.query.name,
+    });
+})
+
 
 app.get("/",(req,res)=>{
-   res.send("welcome to home page of my practice site");
+   res.send("welcome to home page of my practice site")
 })
 
 
 app.get("/about",(req,res)=>{
-    res.send("welcome to about page of my practice site");
+    res.send("welcome to about page of my practice");
 })
 
 app.get('*',(req,res)=>{
